fd7ce9b4b513e5fa8e3bd049238aab789a8c996a
Edvvin <edvvin@mail.com>
2021-11-21 19:33:39 +0100

    dataset fixes

--- datasets.py
+++ datasets.py
@@ -5,9 +5,10 @@ import pandas as pd
 from utils import *
 import re
 
+AA_CNT = 20
 
 def cut_and_pad(x, cutlen):
-    temp = torch.tensor(x[:cutlen]) # cut
+    temp = x[:cutlen] # cut
     padding = cutlen-len(temp)
     return (
             torch.cat([temp, torch.zeros(padding)]), 
@@ -16,8 +17,9 @@ def cut_and_pad(x, cutlen):
 
 class AASequenceDataset(Dataset):
     
-    def __init__(self, tsv_file, maxlen=None):
+    def __init__(self, tsv_file, maxlen=None, equal_size=False):
         self.data = pd.read_csv(tsv_file, sep='\t')
+        self.equal_size = equal_size
         if(maxlen == None):
             self.maxlen = np.max(self.data['seq_len'])
         else:
@@ -31,11 +33,18 @@ class AASequenceDataset(Dataset):
         seq = row['seq']
         seq = re.findall(r'\d+', seq)
         seq = list(map(int, seq))
-        seq, mask = cut_and_pad(seq, self.maxlen)
+        seq = torch.tensor(seq)
+        if(self.equal_size):
+            seq, mask = cut_and_pad(seq, self.maxlen)
+        else:
+            mask = torch.ones(seq.shape[0])
+        onehot_seq = np.zeros(self.maxlen)
+        onehot_seq[arange(0,seq.shape[0] * AA_CNT, AA_CNT) + seq] = 1.0
+        onehot_seq.reshape(seq.shape[0], AA_CNT)
         label = row['Position']
         binlabel = torch.zeros(self.maxlen)
         label = re.findall(r'\d+', label)
         label = list(map(int, label))
         binlabel[label] = 1.
 
-        return seq, mask, binlabel
+        return onehot_seq, mask, binlabel
